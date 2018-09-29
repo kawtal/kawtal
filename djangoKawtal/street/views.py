486a8bfe9b52388161b8ed1d9745dc98b58486d8
Amadou Moctar Diallo <mctrjalloh@gmail.com>
2018-09-29 16:56:12 +0000

    Added simple templates for street app

--- djangoKawtal/street/views.py
+++ djangoKawtal/street/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.http import HttpResponse
+
+posts = [
+    {
+        'author': "Moctar Diallo",
+        'content': "Hello I am looking for a barber",
+        'location': "Medina",
+        'date': "September 29, 2018"
+    },
+    {
+        'author': "Amadou Diallo",
+        'content': "Hello, where is Bus 23",
+        'location': "Dakar Plateau",
+        'date': "September 29, 2018"
+    }
+
+]
+
+
+def home(request):
+    context = {
+        'posts': posts
+    }
+    return render(request, 'street/home.html', context)
