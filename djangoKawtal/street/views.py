4ac0532c6d4170f970135f40f5a5a73c22246522
Amadou Moctar Diallo <mctrjalloh@gmail.com>
2018-10-09 04:23:59 +0000

    Added image property

--- djangoKawtal/street/views.py
+++ djangoKawtal/street/views.py
@@ -5,24 +5,28 @@ from django.http import HttpResponse
 posts = [
     {
         'author': "Samba",
+        'image': "street/img/mctr-fb.jpg",
         'content': "Hello I am looking for a barber",
         'location': "Medina",
         'date': "September 29, 2018"
     },
     {
         'author': "Sira",
+        'image': "",
         'content': "Hello, where is Bus 23",
         'location': "Dakar Plateau",
         'date': "September 29, 2018"
     },
     {
         'author': "Samba",
+        'image': "",
         'content': "I have just lost my pocket",
         'location': "Dakar Plateau",
         'date': "September 29, 2018"
     },
     {
         'author': "Sira",
+        'image': "",
         'content': "Taxi please",
         'location': "Dakar Plateau",
         'date': "September 29, 2018"
